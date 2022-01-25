e03975cefeddbaaeb58868016877f3968999397f
MuhammadHRana <97973572+MuhammadHRana@users.noreply.github.com>
2022-01-25 08:42:53 -0500

    Add files via upload
    Final Version

--- HW.py
+++ HW.py
@@ -2,30 +2,30 @@
 # Create a calculator using functions that asks for two numbers and performs a calculation that the user inputs... Loop until the user chooses not to perform any more calculations.
 
 def calculate(x,y):
-    if x == 'Stop' or y == 'Stop':
+    if x == str('Stop') or y == str('Stop'):
         return "Calculator has been turned off."
     elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
         print('It seems you have inputted something other than an number. Please run script again.')
-        return calculate(input("What is Number 1?"), input("What is number 2?"))
+        return calculate(input("What is Number 1? "), input("What is number 2? "))
     elif (x.isnumeric() or isinstance(float(x), float)) and (y.isnumeric() or isinstance(float(y), float)):
         print(float(x)*float(y))
-        if x == "Stop" or y == "Stop":
+        if x == str('Stop') or y == str('Stop'):
             return "Calculator has been turned off."
         elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
             print('It seems you have inputted something other than an number. Please run script again.')
-            return calculate(input("What is Number 1?"), input("What is number 2?"))
+            return calculate(input("What is Number 1? "), input("What is number 2? "))
         else:
-            if x == "Stop" or y == "Stop":
+            if x == str('Stop') or y == str('Stop'):
                 return "Calculator has been turned off."
             elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
                 print('It seems you have inputted something other than an number. Please run script again.')
-                return calculate(input("What is Number 1?"), input("What is number 2?"))
+                return calculate(input("What is Number 1? "), input("What is number 2? "))
             else:
-                return calculate(input("What is Number 1?"), input("What is number 2?"))
+                return calculate(input("What is Number 1? "), input("What is number 2? "))
     # elif isinstance(x, float) or isinstance(y, float)
     #     print()
 
-calculate(input("What is Number 1?"),input("What is number 2?"))
+calculate(input("What is Number 1? "),input("What is number 2? "))
 # END
 
 # print(isinstance(float(43),float))
