2aec137ab00eb55d223a2638c8d9305debd1758f
MuhammadHRana <97973572+MuhammadHRana@users.noreply.github.com>
2022-01-25 12:02:16 -0500

    Add files via upload
    FINAL: Had to add multiple operator functionality

--- HW.py
+++ HW.py
@@ -8,24 +8,98 @@ def calculate(x,y):
         print('It seems you have inputted something other than an number. Please run script again.')
         return calculate(input("What is Number 1? "), input("What is number 2? "))
     elif (x.isnumeric() or isinstance(float(x), float)) and (y.isnumeric() or isinstance(float(y), float)):
-        print(float(x)*float(y))
-        if x == str('Stop') or y == str('Stop'):
-            return "Calculator has been turned off."
-        elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
-            print('It seems you have inputted something other than an number. Please run script again.')
+        z = input('What operation would you like to use?')
+        if z == str('*'):
+            print(float(x) * float(y))
+            cont = input('Would you like to continue? (Yes/No) ')
+        elif z == str('/'):
+            print(float(x) / float(y))
+            cont = input('Would you like to continue? (Yes/No) ')
+        elif z == str('+'):
+            print(float(x) + float(y))
+            cont = input('Would you like to continue? (Yes/No) ')
+        elif z == str('-'):
+            print(float(x) - float(y))
+            cont = input('Would you like to continue? (Yes/No) ')
+        elif z == str('%'):
+            print(float(x) % float(y))
+            cont = input('Would you like to continue? (Yes/No) ')
+        elif z == str('**'):
+            print(float(x) ** float(y))
+            cont = input('Would you like to continue? (Yes/No) 4')
+        if cont == str('Yes'):
             return calculate(input("What is Number 1? "), input("What is number 2? "))
+        elif cont == str('No'):
+            return
         else:
-            if x == str('Stop') or y == str('Stop'):
-                return "Calculator has been turned off."
-            elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
-                print('It seems you have inputted something other than an number. Please run script again.')
-                return calculate(input("What is Number 1? "), input("What is number 2? "))
-            else:
-                return calculate(input("What is Number 1? "), input("What is number 2? "))
+            print(
+                'It seems you have inputted something other than a proper response. Please run script again.')
+            return calculate(input("What is Number 1? "), input("What is number 2? "))
+
+
+calculate(input("What is Number 1? "), input("What is number 2? "))
+
+
+
+    # if z == str('*') or str('/') or str('-') or str('**') or str('%'):
+    #     if (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
+    #         print('It seems you have inputted something other than an number. Please run script again.')
+    #         return calculate(input("What is Number 1? "), input("What is number 2? "))
+    #     elif (x.isnumeric() or isinstance(float(x), float)) and (y.isnumeric() or isinstance(float(y), float)):
+    #         # var calculation = {
+    #         #     '+': function(x, y)[print(float(x) + float(y))]
+    #         #     '-': function(x, y)[print(float(x) + float(y))]
+    #         #     '*': function(x, y)[print(float(x) + float(y))]
+    #         #     '/': function(x, y)[print(float(x) + float(y))]
+    #         #     '**': function(x, y)[print(float(x) + float(y))]
+    #         #     '%': function(x, y)[print(float(x) + float(y))]
+    #         # }
+    #         # print(calculation[z])
+    #         if z == str('*'):
+    #             print(float(x) * float(y))
+    #             cont = input('Would you like to continue? (Yes/No)')
+    #         elif z == str('/'):
+    #             print(float(x) / float(y))
+    #             cont = input('Would you like to continue? (Yes/No)')
+    #         elif z == str('+'):
+    #             print(float(x) + float(y))
+    #             cont = input('Would you like to continue? (Yes/No)')
+    #         elif z == str('-'):
+    #             print(float(x) - float(y))
+    #             cont = input('Would you like to continue? (Yes/No)')
+    #         elif z == str('%'):
+    #             print(float(x) % float(y))
+    #             cont = input('Would you like to continue? (Yes/No)')
+    #         elif z == str('**'):
+    #             print(float(x) ** float(y))
+    #             cont = input('Would you like to continue? (Yes/No)')
+    #         if cont == str('Yes'):
+    #             return calculate(input("What is Number 1? "), input("What is number 2? "))
+    #         elif cont == str('No'):
+    #             return
+    #         else:
+    #             print('It seems you have inputted something other than a proper response. Please run script again.')
+    #             return calculate(input("What is Number 1? "), input("What is number 2? "))
+    #     if x == str('Stop') or y == str('Stop'):
+    #         return "Calculator has been turned off."
+    #     elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
+    #         print('It seems you have inputted something other than an number. Please run script again.')
+    #         return calculate(input("What is Number 1? "), input("What is number 2? "))
+    #     else:
+    #         if x == str('Stop') or y == str('Stop'):
+    #             return "Calculator has been turned off."
+    #         elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
+    #             print('It seems you have inputted something other than an number. Please run script again.')
+    #             return calculate(input("What is Number 1? "), input("What is number 2? "))
+    #         else:
+    #             return calculate(input("What is Number 1? "), input("What is number 2? "))
+    #     else:
+    #         print('It seems you have inputted something other than an operator. Please run script again.')
+    #         return calculate(input("What is Number 1? "), input("What is number 2? "))
+
     # elif isinstance(x, float) or isinstance(y, float)
     #     print()
- 
-calculate(input("What is Number 1? "),input("What is number 2? "))
+
 # END
 
 # print(isinstance(float(43),float))
