59338ddf35ef38dd25be26249f639ce8d3d99fef
MuhammadHRana <97973572+MuhammadHRana@users.noreply.github.com>
2022-01-25 08:37:07 -0500

    Add files via upload
    Updated HW

--- HW.py
+++ HW.py
@@ -2,43 +2,47 @@
 # Create a calculator using functions that asks for two numbers and performs a calculation that the user inputs... Loop until the user chooses not to perform any more calculations.
 
 def calculate(x,y):
-    if x.isnumeric() == False or y.isnumeric == False or x == '' or y == '':
-        print('It seems you have inputted something other than an integer. Please run script again.')
-        return calculate(input("What is Number 1?"), input("What is number 2?"))
-    elif x == "Stop" or y == "Stop":
+    if x == 'Stop' or y == 'Stop':
         return "Calculator has been turned off."
-    elif x.isnumeric() and y.isnumeric():
-        print(int(x)*int(y))
+    elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
+        print('It seems you have inputted something other than an number. Please run script again.')
+        return calculate(input("What is Number 1?"), input("What is number 2?"))
+    elif (x.isnumeric() or isinstance(float(x), float)) and (y.isnumeric() or isinstance(float(y), float)):
+        print(float(x)*float(y))
         if x == "Stop" or y == "Stop":
             return "Calculator has been turned off."
-        elif x.isnumeric() == False or y.isnumeric == False:
-            print('It seems you have inputted something other than an integer. Please run script again.')
+        elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
+            print('It seems you have inputted something other than an number. Please run script again.')
             return calculate(input("What is Number 1?"), input("What is number 2?"))
         else:
-            if x.isnumeric() == False or y.isnumeric == False:
-                print('It seems you have inputted something other than an integer. Please run script again.')
-                calculate(input("What is Number 1?"), input("What is number 2?"))
+            if x == "Stop" or y == "Stop":
+                return "Calculator has been turned off."
+            elif (x.isnumeric() == False and isinstance(float(x), float) == False) or (y.isnumeric() == False and isinstance(float(y), float) == False) or x == '' or y == '':
+                print('It seems you have inputted something other than an number. Please run script again.')
+                return calculate(input("What is Number 1?"), input("What is number 2?"))
             else:
                 return calculate(input("What is Number 1?"), input("What is number 2?"))
+    # elif isinstance(x, float) or isinstance(y, float)
+    #     print()
 
 calculate(input("What is Number 1?"),input("What is number 2?"))
 # END
 
-
+# print(isinstance(float(43),float))
 
 
 # Problem #2 
 # Create a pyramid of X's for n number of levels.
 
-indent = 0
-def pyramid(total):
-    rows = int(total)
-    indent = rows
-    for number in range(rows):
-        indent += -1
-        print((' '*indent) + number * 'X ')
+# indent = 0
+# def pyramid(total):
+#     rows = int(total)
+#     indent = rows
+#     for number in range(rows):
+#         indent += -1
+#         print((' '*indent) + number * 'X ')
 
 
-pyramid(input('How many rows of X\'s would you like?'))
+# pyramid(input('How many rows of X\'s would you like?'))
 
 # END
